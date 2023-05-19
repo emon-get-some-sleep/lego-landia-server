7bfc6c6d2b1448cd83bc38bef51c1dec94f9d7b7
emonh0que <you@example.com>
2023-05-19 22:15:06 +0600

    successfully added mongodb and done find operation

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 
 require('dotenv').config();
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 // middlware
 
 app.use(express.json());
@@ -14,9 +16,61 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 
+const toys = require('./toys.json');
+
+
 app.get('/', (req, res) => {
     res.send('Lego Landia is online');
 })
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kteeg.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+
+    const legoLibrary = client.db('legoSets').collection('legoCollection');
+
+
+
+
+    app.get('/toys', async(req, res) => {
+        const queryString = req.query.category;
+        const query = {subCategory : queryString};
+        const result = await legoLibrary.find(query).toArray();
+        res.send(result);
+        
+    })
+    
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.listen(port, () => {
